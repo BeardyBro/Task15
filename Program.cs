0b360463e782b2e40dc6ceb34396975a1dcf324c
Andrey Vershinsky <vershinsky90@gmail.com>
2023-06-03 17:18:09 +0300

    task15

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Insert one-digit number");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<1 || num>7)
+{
+    Console.WriteLine("Doesn`t meet the requirements");
+}
+else if (num%7==0 || num%6==0)
+{
+    Console.WriteLine("Yes");
+    }
+            
+else 
+{Console.WriteLine("No");}
